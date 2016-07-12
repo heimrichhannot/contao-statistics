90f33da2dcc9fe8a6b3041ecf865c9d183932948
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-07-12 15:31:04 +0200

    added latest

--- config/config.php
+++ config/config.php
@@ -10,5 +10,5 @@ if (TL_MODE == 'FE')
 		'system/modules/statistics/assets/vendor/Chart.js-2.1.6/dist/Chart.min.js|static';
 
 	$GLOBALS['TL_JAVASCRIPT']['statistics'] =
-		'system/modules/statistics/assets/js/jquery.statistics.js';
+		'system/modules/statistics/assets/js/jquery.statistics.min.js|static';
 }
